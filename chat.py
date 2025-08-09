1ea84a987ae81bb06bbd5488107fbcb8cb65c0fa
ayaalaswad <83227052+ayaalaswad@users.noreply.github.com>
2025-08-09 20:01:34 +0300

    وصف التعديلات

--- chat.py
+++ chat.py
@@ -4,9 +4,14 @@ from graph.graph_builder import build_graph
 graph = build_graph()
 
 while True:
-    question = input("كم api endpoint عنا: ")
+    question = input("ask here: ")
     if question == "exit":
         break
     
     response = graph.invoke({"messages": [("user", question)]})
-    print("الجواب:", response["messages"][-1].content)
+    last_message = response["messages"][-1]
+    if isinstance(last_message, tuple):
+        answer = last_message[1]  # ← الإجابة في العنصر الثاني
+    else:
+        answer = str(last_message)
+    print("الجواب:", answer)
